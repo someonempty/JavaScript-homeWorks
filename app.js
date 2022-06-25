d864636d87f7801697a00a009e4934928674f5f2
Mr-315 <someonempty@gmail.com>
2022-06-25 17:02:53 +0300

    Оптимизирует функцию

--- app.js
+++ app.js
@@ -1,37 +1,12 @@
-const redSquare = document.querySelector('.red-square')
-const blackSquare = document.querySelector('.black-square')
-const yellowSquare = document.querySelector('.yellow-square')
-const orangeSquare = document.querySelector('.orange-square')
-const greenSquare = document.querySelector('.green-square')
 const rectangle = document.querySelector('.rectangle')
 
+const squares = document.querySelectorAll('.square')
 
-redSquare.onclick = () => {
-    if(rectangle.style.backgroundColor != 'red'){
-        rectangle.style.backgroundColor = 'red'
-    }
-}
 
-blackSquare.onclick = () => {
-    if(rectangle.style.backgroundColor != 'black'){
-        rectangle.style.backgroundColor = 'black'
-    }
-}
-
-yellowSquare.onclick = () => {
-    if(rectangle.style.backgroundColor != 'yellow'){
-        rectangle.style.backgroundColor = 'yellow'
-    }
-}
-
-orangeSquare.onclick = () => {
-    if(rectangle.style.backgroundColor != 'orange'){
-        rectangle.style.backgroundColor = 'orange'
-    }
-}
-
-greenSquare.onclick = () => {
-    if(rectangle.style.backgroundColor != 'green'){
-        rectangle.style.backgroundColor = 'green'
-    }
-}
+squares.forEach((element) => {
+    element.onclick = () => {
+          const computedStyles = getComputedStyle(element)
+              
+             rectangle.style.backgroundColor = computedStyles.getPropertyValue('background-color')
+           }
+})
